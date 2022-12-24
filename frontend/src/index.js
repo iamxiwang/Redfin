5af973d0b48d7a4c20e6dc37f0c3579018a38ba8
XiWang <vivian.xi.wang@gmail.com>
2022-12-24 15:04:33 -0800

    Finish set up frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,15 +5,36 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import csrfFetch, { restoreCSRF } from './store/csrf';
+import * as sessionActions from './store/session';
+
 
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
 
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null 
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
 
 function Root() {
   return (
@@ -25,9 +46,10 @@ function Root() {
   );
 }
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
