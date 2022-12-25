98d73a792aec9e64da2d876d4a6bdfc90bed80f7
XiWang <vivian.xi.wang@gmail.com>
2022-12-24 17:05:20 -0800

    added logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from "./components/SignupFormPage";
+
 
 
 function App() {
@@ -8,6 +10,9 @@ function App() {
     <Switch>
       <Route path="/login">
         <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
       </Route>
     </Switch>
   );
